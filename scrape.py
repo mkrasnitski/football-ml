e883769411c83e8385cf1d38bea6a5b993e4c377
Michael Krasnitski <michael.krasnitski@gmail.com>
2019-07-01 21:23:01 -0400

    all stats scraped, ready to be processed

--- scrape.py
+++ scrape.py
@@ -1,22 +1,23 @@
 import requests
 import operator
 import pandas as pd
-from bs4 import BeautifulSoup as bsoup
+from bs4 import BeautifulSoup as bsoup, Comment, SoupStrainer
 from AsyncRequests import async_get
 from dataclasses import dataclass
 from teams import teams
 
-@dataclass
+@dataclass(repr=False)
 class Game:
-	score_info: dict
-	winner: str = None
-	tie: bool = False
+	stats: pd.DataFrame()
 
 	def __post_init__(self):
-		self.tie = len(set(self.score_info.values())) == 1
+		self.tie = len(self.stats['Score'].unique()) == 1
+		self.winner = None
 		if not self.tie:
-			self.winner = max(self.score_info, key=self.score_info.get)
-		
+			self.winner = self.stats['Score'].idxmax()
+
+	def __repr__(self):
+		return f'Game(teams={list(map(str, self.stats.index))}, tie={self.tie}, winner={self.winner})'
 
 year = 2018
 base_url = 'https://www.pro-football-reference.com'
@@ -24,16 +25,17 @@ urls = [f'{base_url}/years/{year}/week_{w}.htm' for w in range(1, 18)]
 
 game_urls = []
 
-for response in async_get(urls, num_workers=17, verbose=0):
+for response in async_get(urls, num_workers=17, verbose=1):
 	soup = bsoup(response.content.decode('utf-8'), 'lxml')
 	games = soup.findAll('div', 'game_summary')
 	for game_table in games:
 		game_urls.append(base_url + game_table.find('td', 'gamelink').find('a')['href'])
-	break
 
-for response in async_get(game_urls, verbose=0):
-	soup = bsoup(response.content.decode('utf-8'), 'lxml')
-	scorebox = soup.find('div', 'scorebox')
+game_stats = []
+
+for i, response in enumerate(async_get(game_urls[:10], verbose=1)):
+	page = response.content.decode('utf-8')
+	scorebox = bsoup(page, 'lxml', parse_only=SoupStrainer('div', 'scorebox'))
 	names = []
 	scores = []
 	for name in scorebox.findAll('strong'):
@@ -43,5 +45,31 @@ for response in async_get(game_urls, verbose=0):
 	for score in scorebox.findAll('div', 'score'):
 		scores.append(score.text)
 
-	score_info = dict(zip(map(teams.get, names), scores))
-	print(Game(score_info))
+	comment = bsoup(page, 'lxml', parse_only=SoupStrainer('div', {'id': 'all_team_stats'}))
+	stats_table = bsoup(comment.find(text = lambda text: isinstance(text, Comment)), 'lxml')
+	row_names = [head.text for head in stats_table.find('thead').findAll('th')[1:]]
+	print(row_names)
+
+	df = pd.DataFrame(index = list(map(teams.__getitem__, row_names)))
+	for name, score in zip(names, scores):
+		df.loc[teams[name], 'Score'] = score
+
+	col_names = ['First Downs', 'Sacked-Yards', 'Total Yards', 'Turnovers', 'Third Down Conv.']
+	for row in stats_table.find('tbody').findAll('tr'):
+		col_name = row.find('th').text
+		if col_name in col_names:
+			df[col_name] = [cell.text for cell in row.findAll('td')]
+	df.rename(columns={'Sacked-Yards': 'Sacks', 'Third Down Conv.': 'Third Down Rate'}, inplace=True)
+	df['Sacks'] = list(reversed(df['Sacks'].str.split('-', expand=True)[0]))
+	third_down = df['Third Down Rate'].str.split('-', expand=True).astype(int)
+	df['Third Down Rate'] = third_down[0] / third_down[1]
+	df[df.keys()[:-1]] = df[df.keys()[:-1]].astype(int)
+
+	g = Game(df)
+	game_stats.append(g.stats)
+
+total_stats = pd.concat(game_stats)
+for team in teams.values():
+	print(team)
+	print(total_stats.loc[team].to_string(index=False))
+	# break
--- teams.py
+++ teams.py
@@ -12,16 +12,19 @@ class Team:
 	def __repr__(self):
 		return f'Team(name={self.name})'
 
+	def __str__(self):
+		return self.name
+
 class TeamDictionary:
 	team_names = {
 		'ARI': 'Arizona Cardinals', 'ATL': 'Atlanta Falcons', 'BAL': 'Baltimore Ravens', 'BUF': 'Buffalo Bills',
 		'CAR': 'Carolina Panthers', 'CHI': 'Chicago Bears', 'CIN': 'Cincinnati Bengals', 'CLE': 'Cleveland Browns',
-		'DAL': 'Dallas Cowboys', 'DEN': 'Denver Broncos', 'DET': 'Detroit Lions', 'GB': 'Green Bay Packers',
-		'HOU': 'Houston Texans', 'IND': 'Indianapolis Colts', 'JAX': 'Jacksonville Jaguars', 'KC': 'Kansas City Chiefs',
+		'DAL': 'Dallas Cowboys', 'DEN': 'Denver Broncos', 'DET': 'Detroit Lions', 'GNB': 'Green Bay Packers',
+		'HOU': 'Houston Texans', 'IND': 'Indianapolis Colts', 'JAX': 'Jacksonville Jaguars', 'KAN': 'Kansas City Chiefs',
 		'LAC': 'Los Angeles Chargers', 'LAR': 'Los Angeles Rams', 'MIA': 'Miami Dolphins', 'MIN': 'Minnesota Vikings',
-		'NE': 'New England Patriots', 'NO': 'New Orleans Saints', 'NYG': 'New York Giants', 'NYJ': 'New York Jets',
+		'NWE': 'New England Patriots', 'NOR': 'New Orleans Saints', 'NYG': 'New York Giants', 'NYJ': 'New York Jets',
 		'OAK': 'Oakland Raiders', 'PHI': 'Philadelphia Eagles', 'PIT': 'Pittsburgh Steelers', 'SEA': 'Seattle Seahawks',
-		'SF': 'San Francisco 49ers', 'TB': 'Tampa Bay Buccaneers', 'TEN': 'Tennessee Titans', 'WAS': 'Washington Redskins',
+		'SFO': 'San Francisco 49ers', 'TAM': 'Tampa Bay Buccaneers', 'TEN': 'Tennessee Titans', 'WAS': 'Washington Redskins',
 	}
 
 	def __init__(self):
@@ -36,5 +39,8 @@ class TeamDictionary:
 			return self.teams[value]
 		else:
 			raise KeyError(f'KeyError: {value}')
+
+	def values(self):
+		return self.teams.values()
 
 teams = TeamDictionary()
